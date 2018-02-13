ed15bd7799528c796e0c521ffcd038fb7d008cf5
Dairo Estiben Beltrán <daiydan99@hotmail.com>
2018-02-13 09:28:54 -0500

    primera clase prog 2
    suma, resta, multiplicacion y division de dos variables

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main{
+	public static void main(String[] arg){
+		System.out.println("");
+		System.out.println("\n\n\t\tEjercicio num1");
+		System.out.println("");
+
+		int var1 = 20;
+		int var2 = 20;
+		System.out.println("la suma de la var1 y la var2 ="+(var1+var2));
+		System.out.println("");		
+		System.out.println("la mul es ="+(var1*var2));
+		System.out.println("");
+		System.out.println("la div es ="+(var1/var2));
+		System.out.println("");
+		System.out.println("la res es ="+(var1-var2));
+		System.out.println("");
+	}	
+}
